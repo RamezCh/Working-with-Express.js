e9af5fc31e9ecf3ac46091e7da19a9406abbe5e9
RamezCh <ramiz_chreide@outlook.com>
2024-09-12 22:50:14 +0300

    Limiting Incoming Requests to Get & POST Separately

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-const express = require('express');
-
+import express from 'express';
+import bodyParser from 'body-parser';
+// Should restart when adding new import, don't depend on nodemon
 const app = express();
 
+// Adding middleware for parsing the body
+// Otherwise we get undefined
+app.use(bodyParser.urlencoded({ extended: false }));
 // Can put use '/' at top to prove that it will match any route & order is important
 // Order of middleware is important
 app.use('/add-product', (req, res, next) => {
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send(
+    '<form method="POST" action="/product"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
+  );
+});
+// app.use executes for both get and post requests
+app.use('/product', (req, res, next) => {
+  // We get object with key value pairs
+  console.log(req.body);
+  // redirects to / route
+  res.redirect('/');
+});
+// app.get executes only for get requests
+app.get('/product', (req, res, next) => {
+  // We get object with key value pairs
+  console.log(req.body);
+  // redirects to / route
+  res.redirect('/');
+});
+
+// app.post executes only for post requests
+app.post('/product', (req, res, next) => {
+  // We get object with key value pairs
+  console.log(req.body);
+  // redirects to / route
+  res.redirect('/');
 });
 
 // Request goes through the middleware from top to bottom
