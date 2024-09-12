ef538d7e08b0b7f9cbd201ed6e5f723b1a3dcc70
RamezCh <ramiz_chreide@outlook.com>
2024-09-12 23:07:33 +0300

    Adding 404 Page

--- app.js
+++ app.js
@@ -14,4 +14,9 @@ app.use(adminRoutes);
 // order matters, this will never be reached if it is placed before adminRoutes but in this case it will because we are using get here and post in adminRoutes and not .use
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  // 404 = page not found code
+  res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(3000);
