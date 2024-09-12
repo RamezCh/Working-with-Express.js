9824e8823705c6644125099dc689fb7bfdf67167
RamezCh <ramiz_chreide@outlook.com>
2024-09-12 23:11:13 +0300

    Filtering Paths

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // adminRoutes is a middleware function, remember order matters
-app.use(adminRoutes);
+app.use('/admin', adminRoutes); // filter paths, only paths starting with /admin will be handled by adminRoutes
+
 // order matters, this will never be reached if it is placed before adminRoutes but in this case it will because we are using get here and post in adminRoutes and not .use
 app.use(shopRoutes);
 
--- routes/admin.js
+++ routes/admin.js
@@ -6,11 +6,11 @@ const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
   res.send(
-    '<form method="POST" action="/product"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
+    '<form method="POST" action="/admin/add-product"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
   );
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
