7c3b2da8ea93faf534566e5d2bd052d594e817b5
RamezCh <ramiz_chreide@outlook.com>
2024-09-12 23:33:35 +0300

    Returning 404 Page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   // 404 = page not found code
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
