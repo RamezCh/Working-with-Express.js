a0c178f86c782e97caac664ea849f8d5c9bdf9fb
RamezCh <ramiz_chreide@outlook.com>
2024-09-13 22:44:41 +0300

    Serving Files Statically

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+// Takes static path requests and forwards them to the public folder
+// Allows us to link CSS in HTML files without having to specify the full path
+app.use(express.static(path.join(__dirname, 'public'))); // We can have multiple static folders and it will go through each of them until it gets a valid response
 
 // adminRoutes is a middleware function, remember order matters
 app.use('/admin', adminRoutes); // filter paths, only paths starting with /admin will be handled by adminRoutes
